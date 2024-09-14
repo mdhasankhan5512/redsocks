776fdb7f33f8ca0727d7048b55453ad04da8d7e3
mdhasankhan5512 <131230205+mdhasankhan5512@users.noreply.github.com>
2024-09-14 20:06:39 +0600

    Update proxy_setup.sh

--- proxy_setup.sh
+++ proxy_setup.sh
@@ -5,9 +5,9 @@ opkg install redsocks iptables-nft iptables-mod-nat-extra
 sleep 2
 rm /etc/init.d/redsocks
 rm /etc/redsocks.conf
-https://raw.githubusercontent.com/mdhasankhan5512/redsocks/main/excludetiktok
+https://raw.githubusercontent.com/mdhasankhan5512/redsocks/refs/heads/main/redsocks
 sleep 2
-mv exlcudetiktok redsocks
+# mv exlcudetiktok redsocks
 wget https://raw.githubusercontent.com/mdhasankhan5512/redsocks/main/redsocks.conf
 mv redsocks /etc/init.d/
 chmod +x /etc/init.d/redsocks
